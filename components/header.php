f41815f62d70fa79a28bdd656d5f117aa4f5673c
Kelly <kellyngui2006@gmail.com>
2026-04-07 18:16:52 +0800

    upda

--- components/header.php
+++ components/header.php
@@ -24,7 +24,7 @@ $categories = getAllCategories();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= 'NOAIR | '.($_title ?? 'HOME') ?></title>
     <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/components/header.css">
+    <link rel="stylesheet" href="../css/merged.css">
 </head>
 <body>
 
